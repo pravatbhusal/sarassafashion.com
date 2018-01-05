84ca27db0c92e0b8b1215363bf9c232f97aa8a5f
Pravat Bhusal <pravat.bhusal@gmail.com>
2018-01-04 21:55:05 -0600

    Add item buttons and add 1 event into the event page

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Sarassa Fasion
-An e-commerce website programmed for Sal Khan's fashion business.
+# SarassA Fashion
+An e-commerce website programmed for the SarassA Fashion's business. The website program  
+is rightfully open-source for anybody to utilize and study. Please note that the website  
+files and directories in the live website were re-configured for security reasons.  
+
+# Technologies
+- HTML5
+- CSS3/MaterializeCSS
+- Javascript/jQuery
+- PHP 7.0
+- MySQL on MariaDB
+
+# Documentation and Setup
---
+++ events.php
@@ -0,0 +1,64 @@
+<html>
+<body>
+<header>
+	<title>Events | Buy Cultural Fashion Online USA</title>     
+	<!--style.css, favcon, googlefont, materializecss-->
+	<link href="styles/events_style.css" type="text/css" rel="stylesheet">       
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">  	
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+	
+	<!--jquery, materializejs-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+	
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<meta name="description" content=""> 
+	<meta name="author" content=""> 
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
+	
+	<!--navbar-->
+	<div id="navbarHeader"></div>
+	<div id="navbarTop">
+		<a target="_blank" href="https://www.facebook.com/sarassafashion" style="margin-right: 5px"><img width="18" height="18" src="rsrc/index/facebook.png"></a>
+		<a target="_blank" href="https://www.instagram.com/sarassa_official/" style="margin-right: 5px"><img width="18" height="18" src="rsrc/index/instagram.jpg"></a>
+		<a target="_blank" href="" style="margin-right: 100px;"><img width="18" height="18" src="rsrc/index/twitter.jpg"></a>
+	</div>
+	<div id="navbarMiddle">
+		<a href="index.php"><p class="center-align" id="titleText">SarassA Fashion</p></a>
+	</div>
+	<div id="navbarBottom">
+		<h6 style="margin-right: 123px; color: white;">Search</h6>
+	</div>
+	
+	<!--navbar and body white seperation-->
+	<div id="seperator"></div>
+</header>
+
+<main>
+	<!-- Item Buttons -->
+	<div class="row" style="margin-top: 10px;" align="center">
+		<a href="" style="margin-right: 20px;" class="waves-effect waves-light btn" id="itemBTN">New Arrivals</a>
+		<a href="" style="margin-right: 20px;" class="waves-effect waves-light btn" id="itemBTN">Women's Clothing</a>
+		<a href="" style="margin-right: 20px;" class="waves-effect waves-light btn" id="itemBTN">Men's Clothing</a>
+		<a href="" style="margin-right: 20px;" class="waves-effect waves-light btn" id="itemBTN">Jewelry</a>
+		<a href="" style="margin-right: 20px;" class="waves-effect waves-light btn" id="itemBTN">Sales</a>
+		<a href="events.php" style="margin-right: 20px;" class="waves-effect waves-light btn" id="itemBTN">Events</a>
+	</div>
+		
+	<!-- Events -->
+	<div class="row">
+		<img class="col s4" id="event">
+		<img src="rsrc/events/event1.jpg" class="col s4" id="event">
+		<img class="col s4" id="event">
+	</div>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
